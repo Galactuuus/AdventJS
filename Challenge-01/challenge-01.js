08566a16d387c188d5c8946b907e6bc94a5ce539
Galactuuus <galactuuus@gmail.com>
2021-12-26 12:03:29 +0100

    Challenge 01 complete

---
+++ Challenge-01/challenge-01.js
@@ -0,0 +1,18 @@
+const ovejas = [
+    { name: 'Noa', color: 'azul' },
+    { name: 'Euge', color: 'rojo' },
+    { name: 'Navidad', color: 'rojo' },
+    { name: 'Ki Na Ma', color: 'rojo' },
+    { name: 'AAAAAaaaaa', color: 'rojo' },
+    { name: 'Nnnnnnnn', color: 'rojo' }
+];
+
+export default function contarOvejas(ovejas) {
+
+    return ovejas.filter( oveja => {
+        oveja.name.toLowerCase().includes('a') && oveja.name.toLowerCase().includes('n') &&
+        oveja.color === 'rojo';
+    });
+}
+
+console.log(contarOvejas(ovejas));
